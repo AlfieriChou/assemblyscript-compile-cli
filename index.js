66f42d3cae25d12ef88ea8798b7899d7ddd22a29
AlfieriChou <alfierichou@gmail.com>
2021-06-15 17:20:51 +0800

    chore: add ascPath

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const shell = require('shelljs')
 const assert = require('assert')
 const readDirFileNames = require('read-dir-filenames')
 const path = require('path')
+const fs = require('fs')
 
 const rootPath = process.cwd()
 
@@ -12,16 +13,25 @@ const {
 
 assert(MODEL_PATH, 'MODEL_PATH is required')
 
+const ascPath = `${rootPath}/node_modules/.bin/asc`
+
+if (!fs.existsSync(ascPath)) {
+  shell.echo(`Error: please install assemblyscript!`)
+  shell.exit(1)
+}
+
 const models = readDirFileNames(MODEL_PATH)
 
 models.reduce(async (promise, asPath, index) => {
   await promise
   const filename = path.parse(asPath).name
-  const runCommandRet = shell.exec(`${rootPath}/node_modules/.bin/asc ${asPath} -o ${COMPILE_PATH}/${filename}.wasm`)
+  const runCommandRet = shell.exec(`
+    ${ascPath} ${asPath} -o ${COMPILE_PATH}/${filename}.wasm
+  `)
   if (runCommandRet.code !== 0) {
     shell.echo(`Error: ${filename} compile failed!`)
   }
-  if (index === models.length) {
+  if (index + 1 === models.length) {
     shell.exit(1)
   }
 }, Promise.resolve())
