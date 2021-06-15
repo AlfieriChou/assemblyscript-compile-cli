0c2ceb7ec854639da5dbd22da05f95a51205acc4
AlfieriChou <alfierichou@gmail.com>
2021-06-15 17:01:05 +0800

    fix: exit shell

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ const assert = require('assert')
 const readDirFileNames = require('read-dir-filenames')
 const path = require('path')
 
+const rootPath = process.cwd()
+
 const {
   MODEL_PATH,
-  COMPILE_PATH = `${process.cwd()}/compiles`,
+  COMPILE_PATH = `${rootPath}/compiles`,
 } = process.env
 
 assert(MODEL_PATH, 'MODEL_PATH is required')
 
 const models = readDirFileNames(MODEL_PATH)
 
-models.reduce(async (promise, asPath) => {
+models.reduce(async (promise, asPath, index) => {
   await promise
   const filename = path.parse(asPath).name
-  const runCommandRet = shell.exec(`asc ${asPath} -o ${COMPILE_PATH}/${filename}.wasm`)
+  const runCommandRet = shell.exec(`${rootPath}/node_modules/.bin/asc ${asPath} -o ${COMPILE_PATH}/${filename}.wasm`)
   if (runCommandRet.code !== 0) {
     shell.echo(`Error: ${filename} compile failed!`)
   }
+  if (index === models.length) {
+    shell.exit(1)
+  }
 }, Promise.resolve())
-
-shell.exit(1)
